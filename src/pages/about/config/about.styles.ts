886546de30698ad07f10898d311dee1117f9806f
Сергей <polwen@yandex.ru>
2025-05-06 14:45:39 +0300

    about page updated

--- src/pages/about/config/about.styles.ts
+++ src/pages/about/config/about.styles.ts
@@ -3,6 +3,7 @@ const themeLight = {
     textAccent: "text-gray-800",
     textDefault: "text-gray-700",
     border: "border-pink-100",
+    containerBg: "bg-white/20",
 };
 
 const themeDark = {
@@ -10,10 +11,12 @@ const themeDark = {
     textAccent: "text-white",
     textDefault: "text-gray-300",
     border: "border-gray-700",
+    containerBg: "bg-gray-800/40"
 };
 
 const createAboutTheme = (theme: typeof themeLight) => ({
-    wrapper: `max-w-2xl mx-auto px-4 py-8 ${theme.textDefault}`,
+    // wrapper: `max-w-2xl mx-auto px-4 py-8 ${theme.textDefault}`,
+    wrapper: `w-full lg:w-1/2 max-w-2xl mx-auto py-8 px-4 text-base leading-relaxed shadow rounded-xl ${theme.textDefault} ${theme.containerBg} ${theme.textDefault}`,
     title: `text-3xl font-bold mb-6 text-center ${theme.textAccent}`,
     paragraph: "space-y-4 text-lg",
 });
--- src/pages/about/ui/AboutPage.tsx
+++ src/pages/about/ui/AboutPage.tsx
@@ -4,18 +4,19 @@ import {localization} from "@/shared/config/localization";
 import {about} from "@/pages/about/config/about.styles";
 
 export const AboutPage: FC = () => {
-    const {appTheme} = useTheme();
-
+    const { appTheme } = useTheme();
     const s = about[appTheme];
 
     return (
         <div className={s.wrapper}>
             <h1 className={s.title}>{localization.ru.about.title}</h1>
-            <div className={s.paragraph}>
-                {localization.ru.about.paragraphs.map((text, index) => (
-                    <p key={index}>{text}</p>
+            <div className={`${s.paragraph} text-base [&_a]:text-blue-600 [&_a:hover]:text-blue-800`}>
+                {localization.ru.about.paragraphs.map((html, index) => (
+                    <p key={index} dangerouslySetInnerHTML={{ __html: html }} />
                 ))}
             </div>
+
         </div>
     );
 };
+
