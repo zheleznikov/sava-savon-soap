fcbbac3cc7ec306426be8da3ca6fe650e2e2a521
Сергей <polwen@yandex.ru>
2025-05-11 23:01:39 +0300

    import fixed

--- src/pages/calculator/model/useHydrateRecipeFromLocation.ts
+++ src/pages/calculator/model/useHydrateRecipeFromLocation.ts
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import {useSoapRecipe} from "../../../feature/recipe-calculation";
+// import {useSoapRecipe} from "../../../feature/recipe-calculation";
 import {oils} from "../../../entities/oil/model/oils";
 import {hydrateRecipeFromState} from "../libs/hydrateRecipeFromState";
 
